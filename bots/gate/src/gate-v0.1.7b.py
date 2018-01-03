377fe41c91ccaefe86df7f187153ba10a87f4fe4
anakratis <michael.chamerski@gmail.com>
2018-01-02 22:20:44 -0800

    Update and rename gate-v0.1.3b.py to gate-v0.1.7b.py

--- bots/gate/src/gate-v0.1.7b.py
+++ bots/gate/src/gate-v0.1.7b.py
@@ -1,15 +1,19 @@
 ####################################################################
 # Automated Stoploss for Bittrex
+#
 # Author:   Anakratis / Vavrespa
 # NOTE:     You must have an API key assigned, with trading and
 #           viewing privileges.
+#
 ####################################################################
 
+import sys
 import time
+import datetime
 import settings
 import btrxapi
 
-from btrxapi import API_V1_1, API_V2_0
+from btrxapi import API_V1_1, API_V2_0, BUY_ORDERBOOK
 from settings import V1Bittrex, V2Bittrex
 
 print("")
@@ -23,6 +27,9 @@ print("")
 print("Launching...")
 
 time.sleep(0.5)
+
+print("")
+APIKEY, SECRETKEY = settings.api_settings()
 
 print("")
 MARKET = settings.market_settings()
@@ -56,17 +63,26 @@ print("")
 
 time.sleep(0.5)
 
+RetryCount = 0
 MarketString = "BTC-" + MARKET
 print(MarketString + " STOP : " + str(STOP) + " BTC")
 
 while True:
-    MarketBalanceArray = V2Bittrex.get_balance(MARKET)
-    MarketBalance = MarketBalanceArray['result']['Available']
-    
     TradePriceArray = V1Bittrex.get_ticker(MarketString)
     TradePrice = float(TradePriceArray['result']['Last'])
+    CurrentTime = str(datetime.datetime.now())
 
-    if TradePrice <= STOP:
+    print(MarketString + " LAST : " + str(TradePrice) + " BTC" + " " + CurrentTime + " GMT", end="\r")
+    sys.stdout.flush()
+
+    if TradePrice <= float(STOP):
+
+        print("")
+        print("Activating stoploss...")
+
+        MarketBalanceArray = V2Bittrex.get_balance(MARKET)
+        MarketBalance = MarketBalanceArray['result']['Available']
+
         if MarketBalance > 0:
             OpenOrders = []
             OpenOrdersArray = V2Bittrex.get_open_orders(MarketString)
@@ -76,17 +92,35 @@ while True:
                 for i in range(0, OpenOrdersNumber):
                     OpenOrdersIter = OpenOrdersArray['result'][i]['OrderUuid']
                     print(OpenOrdersIter)
+
                     test = V1Bittrex.cancel(OpenOrdersIter)
                     print(test)
 
-            print("")
-            print("All open orders in current market have been canceled.")
-            print("")
+                    print("")
+                    print("All open orders in current market have been canceled.")
+                    print("")
 
             MarketSell = V1Bittrex.get_orderbook(MarketString, depth_type=BUY_ORDERBOOK)
             MarketSellAmt = float(MarketSell['result'][-1]['Rate'])
 
             MarketSellOrder = V1Bittrex.sell_limit(MarketString, float(MarketBalance), MarketSellAmt)
+
+            if MarketSellOrder['success'] == False:
+                ErrorMessage = MarketSellOrder['message']
+                print("")
+                print("ERROR: " + ErrorMessage)
+                RetryCount = RetryCount + 1
+
+                if RetryCount == 3:
+                    print("3/3 sell attempts have failed. Closing Gate...")
+                    print("")
+                    quit()
+
+                else:
+                    print(str(RetryCount) + "/3 sell attempts have failed. Retrying...")
+                    print("")
+                    continue
+
             StoplossUUID = MarketSellOrder['result']['uuid']
             StoplossOrder = V1Bittrex.get_order(StoplossUUID)
 
@@ -100,5 +134,4 @@ while True:
                     ActualRate = StoplossOrder['result']['Price']
                     print("STOPLOSS activated.")
                     print("SOLD " + MarketBalance + " " + MARKET + " at " + str(ActualRate) + " BTC")
-                    print("")
                     quit()
