b4f13b81fc46151c6e365ee336617e103006a226
verata-veritatis <mchamerski@protonmail.com>
2019-06-01 01:46:54 -0700

    Update and rename bots/binance/nbinance-autostop.py to bots/binance/nbinance-autostop/nbinance-autostop.py

--- bots/binance/nbinance-autostop/nbinance-autostop.py
+++ bots/binance/nbinance-autostop/nbinance-autostop.py
@@ -1,7 +1,9 @@
 #######################################
 # NODAL.ID BINANCE AUTOSTOP
-# Version x1.17 (May 30, 2019)
+# Version x1.21 (June 1, 2019)
 #######################################
+
+# REQUIREMENTS: python3.7 python-binance colorama
 
 import os
 import json
@@ -12,8 +14,6 @@ from binance import exceptions
 
 import threading
 import requests
-
-import numpy as np
 import operator
 
 import textwrap
@@ -26,8 +26,6 @@ import sys
 #######################################
 # LOGGING FUNCTION
 #######################################
-
-warnings.filterwarnings("ignore")
 
 def Logging(Level, String):
 
@@ -56,16 +54,19 @@ def Logging(Level, String):
 # API KEY SETTINGS
 #######################################
 
+# DETERMINE FOLDER PATH
 PATH = os.path.expanduser('~/.ndar/')
 if not os.path.exists(PATH):
     os.makedirs(PATH)
 
+# OPEN CONFIG FILE
 try:
     with open(PATH + "binance.cfg", 'r') as CONFIG:
         USER = json.load(CONFIG)
         API_KEY = USER['APIK']
         API_SECRET = USER['SECK']
 
+# IF NOT, CREATE CONFIG FILE
 except:
     print("*** No local key file found.")
     time.sleep(1)
@@ -87,6 +88,7 @@ except:
 # IP ADDRESS POLLING
 #######################################
 
+# FETCH IP ADDRESS FROM ICANHAZIP
 try:
     IP_TEST = requests.get(url = 'https://icanhazip.com')
 except:
@@ -96,11 +98,13 @@ except:
     time.sleep(1)
     sys.exit()
 
+# DECODE IP ADDRESS
 IP_TEST = IP_TEST.content
 IP_TEST = IP_TEST.decode()
 
+# PRINT IP ADDRESS & ASK TO PROCEED
 print("")
-GATE = input("{}WARNING: Your IP address has been recorded as {}. Would you like to continue? (Y or N):{} ".format(Back.BLUE, IP_TEST.rstrip(), Style.RESET_ALL))
+GATE = input("{}WARNING: Your IP address has been detected as {}. Would you like to continue? (Y or N):{} ".format(Back.BLUE, IP_TEST.rstrip(), Style.RESET_ALL))
 if GATE.upper() == 'Y':
     pass
 else:
@@ -115,7 +119,7 @@ else:
 
 print("")
 Logging(0, "NODAL : Binance : Autostop Bot")
-Logging(0, "x1.17 May 30, 2019")
+Logging(0, "x1.21 June 1, 2019")
 time.sleep(1)
 
 Logging(0, "Connecting to wss://stream.binance.com:9443...")
@@ -134,40 +138,53 @@ print("")
 
 def UserQuery(StopList):
 
-    # Ticker & TickerSelf
+    # TICKER & TICKERSELF
     while True:
         Ticker = input("Enter the Binance pair (e.g. NEOBTC), type 'all' to show all active stops, or type 'rm' to remove a stop: ")
         Ticker = Ticker.upper()
+
         if Ticker == 'EXIT':
             print("")
             print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
             sys.exit()
+
         elif Ticker == 'ALL':
             print("")
             print("----------------------------------------------------------------------------")
             StopNr = 0
+
             for Stop in StopList:
                 StopNr = StopNr + 1
+
                 if Stop['type'] == 0:
                     FrmCd = 'TSSL'
+
                 else:
                     FrmCd = 'STOP'
+
                 if not Stop['status']:
                     SttCd = Fore.GREEN + "ACTIVE" + Style.RESET_ALL
+
                 else:
                     SttCd = Fore.LIGHTRED_EX + "TRIGGERED" + Style.RESET_ALL
+
                 print(str(StopNr) + ": " + SttCd + " - {} for {} {} at {}.".format(FrmCd, str(Stop['qty']), Stop['ticker'], str(Stop['price'])))
+
             print("----------------------------------------------------------------------------")
             print("")
             continue
+
         elif Ticker == 'RM':
             print("")
             print("----------------------------------------------------------------------------")
+
             RmdEntry = input("Which entry do you want to remove? (e.g. '4'): ")
             RmdEntry = int(RmdEntry) - 1
+
             print("{}DELETED{}: {}".format(Fore.LIGHTRED_EX, Style.RESET_ALL, StopList[RmdEntry]))
             print("----------------------------------------------------------------------------")
             print("")
+
             del StopList[RmdEntry]
             continue
         
@@ -176,6 +193,7 @@ def UserQuery(StopList):
         if SanityCheck != None:
             TickerSelf = SanityCheck['baseAsset']
             break
+
         else:
             Logging(2, "Invalid entry. Please try again")
             continue
@@ -183,7 +201,7 @@ def UserQuery(StopList):
     print("")
     print("---------------------------------- {}{}{} ----------------------------------".format(Fore.LIGHTYELLOW_EX, Ticker, Style.RESET_ALL))
 
-    # Grab recent trades
+    # GRAB RECENT TRADES
     Trades = Binance.get_my_trades(symbol=Ticker)
 
     try:
@@ -191,164 +209,215 @@ def UserQuery(StopList):
         RecentPrice = float(RecentTrade['price'])
         RecentQty = float(RecentTrade['qty'])
         RefQuery = input("Recent trade detected for {} {} at {}. Would you like to use this as a reference? (Y or N): ".format(RecentQty, TickerSelf, RecentPrice))
+
     except:
         RefQuery = 'N'
 
-    # Type
+    # TYPE
     while True:
         print("""
-Would you like to place a trailing stop/stop-loss or stoploss only?")
-    0: Trailing Stop + Stop-loss")
+Would you like to place a trailing stop/stop-loss or stoploss only?
+    0: Trailing Stop + Stop-loss
     1: Stop-loss Only
         """)
         Type = input("*** Selection: ")
+
         try:
             Type = int(Type)
+            
         except:
             try:
                 if Type.upper() == 'EXIT':
                     print("")
                     print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
                     sys.exit()
+
             except:
                 Logging(2, "Invalid entry. Please try again")
                 continue
+
             Logging(2, "Invalid entry. Please try again")
             continue
+
         if Type == 0 or Type == 1:
             break
+
         else:
             Logging(2, "Invalid entry. Please try again")
             continue
 
-    # Price, Trail, & Qty
+    # PRICE, TRAIL, QTY
     while True:
         if RefQuery.upper() == 'EXIT':
             print("")
             print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
             sys.exit()
-        elif RefQuery.upper() == 'Y':
 
+        elif RefQuery.upper() == 'Y':
             Qty = RecentQty
 
             if Type == 0:
                 while True:
                     Trail = input("By what percentage do you want to trail (e.g. '1%')?: ")
+
                     if Trail[-1] == '%':
                         Trail = Trail[:-1]
+
                     Price = RecentPrice - (RecentPrice * (float(Trail) / 100))
+                    print(Price)
+
                     if Trail.upper() == 'EXIT':
                         print("")
                         print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
                         sys.exit()
+
                     try:
                         float(Trail)
                         break
+
                     except:
                         Logging(2, "Invalid entry. Please try again")
                         continue
+
                     break
+
             elif Type == 1:
                 while True:
                     Price = input("At what price (in BTC) or percentage from entry do you want to place the stop (e.g. '0.00004806' or '5%')?: ")
                     Trail = None
+
                     if Price == 'EXIT' or Price == 'exit':
                         print("")
                         print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
                         sys.exit()
+
                     try:
                         if Price.endswith('%'):
                             Price = Price[:-1]
                             Price = RecentPrice - (RecentPrice * (float(Price) / 100))
+
                         float(Price)
                         break
+
                     except:
                         Logging(2, "Invalid entry. Please try again")
                         continue
+
                     break
+
         elif RefQuery.upper() == 'N':
             if Type == 0:
                 while True:
                     Price = input("What is your entry price (e.g. 0.00004608)?: ")
+
                     if Price.upper() == 'EXIT':
                         print("")
                         print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
                         sys.exit()
+
                     try:
                         float(Price)
                         break
+
                     except:
                         Logging(2, "Invalid price. Please try again")
                         continue
+
                     break
 
                 while True:
                     Trail = input("By what percentage do you want to trail (e.g. '1%')?: ")
+
                     if Trail.upper() == 'EXIT':
                         print("")
                         print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
                         sys.exit()
+
                     if Trail.endswith('%'):
                         Trail = Trail[:-1]
+
                     try:
                         float(Trail)
+                        Price = float(Price) - (float(Price) * (float(Trail) / 100))
+                        print(Price)
                         break
+
                     except:
                         Logging(2, "Invalid entry. Please try again")
                         continue
+
                     break
+
                 # Quantity
                 while True:
                     Qty = input("Enter the desired quantity or type 'all' to use entire available {} balance: ".format(TickerSelf))
+
                     if Qty.upper() == 'EXIT':
                         print("")
                         print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
                         sys.exit()
+
                     elif Qty.upper() == 'ALL':
                         Asset = Binance.get_asset_balance(TickerSelf)
                         Qty = Asset['free']
+
                     else:
                         try:
                             float(Qty)
                             break
+
                         except:
                             Logging(2, "Invalid entry. Please try again")
                             continue
+
                         break
+
                     break
+
             elif Type == 1:
                 while True:
                     Price = input("At what price (in BTC) do you want to place the stop (e.g. 0.00004806): ")
                     Trail = None
+
                     if Price.upper() == 'EXIT':
                         print("")
                         print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
                         sys.exit()
+
                     try:
                         float(Price)
                         break
+
                     except:
                         Logging(2, "Invalid price. Please try again")
                         continue
+
                     break
+
                 # Quantity
                 while True:
                     Qty = input("Enter the desired quantity or type 'all' to use entire available {} balance: ".format(TickerSelf))
+
                     if Qty.upper() == 'EXIT':
                         print("")
                         print("{}NOTE: The program has shut down. You may now close this window.{}".format(Back.RED, Style.RESET_ALL))
                         sys.exit()
+
                     elif Qty.upper() == 'ALL':
                         Asset = Binance.get_asset_balance(TickerSelf)
                         Qty = Asset['free']
+
                     else:
                         try:
                             float(Qty)
                             break
+
                         except:
                             Logging(2, "Invalid entry. Please try again")
                             continue
+
                         break
+
         else:
             Logging(2, "Invalid entry. Please try again")
             continue
@@ -358,16 +427,23 @@ Would you like to place a trailing stop/stop-loss or stoploss only?")
     print("----------------------------------------------------------------------------")
     print("")
 
+    # SPECIFY ORDER SIZE PRECISION
+    #########################################################
     sPrecision = Binance.get_symbol_info(symbol=Ticker)
     sPrecision = sPrecision['filters'][2]['minQty']
     sPrecision = Decimal(sPrecision).normalize()
-    Qty = Qty // float(sPrecision) * float(sPrecision)
+    Qty = float(Qty) // float(sPrecision) * float(sPrecision)
+    #########################################################
 
     return Type, Ticker, TickerSelf, Price, Trail, Qty
 
 #######################################
 # STOP LIST MANAGERS
 #######################################
+
+def Update():
+    while True:
+        StopList.append(Application(StopList))
 
 def Application(StopList):
     T, K, S, P, R, Q = UserQuery(StopList)
@@ -380,28 +456,32 @@ def Process(StopList):
                 if not Stop['status']:
                     QuoteArray = Binance.get_ticker(symbol=Stop['ticker'])
                     LastPrice = float(QuoteArray['lastPrice'])
+
                     if Stop['type'] == 0:
                         TrailingStop = LastPrice - (LastPrice * (Stop['trail'] / 100))
+
                         if TrailingStop > Stop['price']:
                             Stop['price'] = TrailingStop
+
                         else:
                             if LastPrice <= Stop['price']:
                                 try:
                                     Binance.order_market_sell(symbol=Stop['ticker'], quantity=Stop['qty'])
+
                                 except exceptions.BinanceAPIException:
                                     print("{}FATAL: Binance API error detected. Your trade amount is most likely below the notional minimum. Please double check your assets.{}".format(Back.RED, Style.RESET_ALL))
+
                                 Stop['status'] = True
+
                     else:
                         if LastPrice <= Stop['price']:
                             try:
                                 Binance.order_market_sell(symbol=Stop['ticker'], quantity=Stop['qty'])
+
                             except exceptions.BinanceAPIException:
                                 print("{}FATAL: Binance API error detected. Your trade amount is most likely below the notional minimum. Please double check your assets.{}".format(Back.RED, Style.RESET_ALL))
-                            Stop['status'] = True
 
-def Update():
-    while True:
-        StopList.append(Application(StopList))
+                            Stop['status'] = True
 
 #######################################
 # PRIMARY THREAD
